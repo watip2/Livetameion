8b53d048bdf29d9bedf0b5d2c42c08c51e27fa07
mhsohail <sohailx2x@yahoo.com>
2015-10-21 13:33:05 +0500

    VendorMembershipPluginPart1Completed

---
+++ Plugins/Nop.Plugin.Misc.VendorMembership/Domain/Vendorrr.cs
@@ -0,0 +1,16 @@
+﻿using Nop.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Misc.VendorMembership.Domain
+{
+    public class Vendorrr : BaseEntity
+    {
+        public int VendorrrId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Plugins/Nop.Plugin.Misc.VendorMembership/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Autofac.Core;
+using Nop.Core.Data;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Web.Framework.Mvc;
+using Nop.Plugin.Misc.VendorMembership.Data;
+
+namespace Nop.Plugin.Misc.VendorMembership.Domain.Infrastructure
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        private const string CONTEXT_NAME = "nop_object_context_product_view_tracker";
+
+        public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder)
+        {
+            //data context
+            this.RegisterPluginDataContext<VendorrrObjectContext>(builder, CONTEXT_NAME);
+
+            //override required repository with our custom context
+            builder.RegisterType<EfRepository<Vendorrr>>()
+                .As<IRepository<Vendorrr>>()
+                .WithParameter(ResolvedParameter.ForNamed<IDbContext>(CONTEXT_NAME))
+                .InstancePerLifetimeScope();
+        }
+
+        public int Order
+        {
+            get { return 1; }
+        }
+    }
+}
