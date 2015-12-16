5b7888f9ad0a61a46fd7a6df4290457ac218b920
mhsohail <sohailx2x@yahoo.com>
2015-12-16 16:07:17 +0500

    sales commission settings

---
+++ Plugins/Nop.Plugin.Misc.VendorMembership/Domain/SalesCommissionSettings.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Misc.VendorMembership.Domain
+{
+    public class SalesCommissionSettings : ISettings
+    {
+    }
+}
---
+++ Plugins/Nop.Plugin.Misc.VendorMembership/ViewModels/MultitenancySettingsModel.cs
@@ -0,0 +1,26 @@
+﻿using Nop.Web.Framework.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Misc.VendorMembership.ViewModels
+{
+    public class MultitenancySettingsModel : BaseNopModel
+    {
+        public MultitenancySettingsModel()
+        {
+            SalesCommissionSettings = new SalesCommissionSettingsModel();
+        }
+        public SalesCommissionSettingsModel SalesCommissionSettings { get; set; }
+
+        #region Nested classes
+
+        public partial class SalesCommissionSettingsModel : BaseNopModel
+        {
+            
+        }
+        #endregion
+    }
+}
